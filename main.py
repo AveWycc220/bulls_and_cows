eca264d04fe7b844fe72786a76cf1b7a4ba117bf
AveWycc220 <mik16748@gmail.com>
2020-05-18 14:05:47 +0300

    Feat (modules): Add method, that generate sequence

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 from modules.bulls_and_cows.bulls_and_cows import BullsAndCows
 
+A = BullsAndCows('1256')
+print(A.status)
+print(A._BullsAndCows__computers_sequence)
--- modules/bulls_and_cows/bulls_and_cows.py
+++ modules/bulls_and_cows/bulls_and_cows.py
@@ -1,6 +1,7 @@
 """ Module of class : BullsAndCows """
 import random
 
+
 class BullsAndCows(): 
     """ Class for playing in Bulls and Cows """ 
     __players_sequence = None
@@ -8,10 +9,17 @@ class BullsAndCows():
     status = "Game is not initialized"
     def __init__(self, input_sequence):
         """ Init game-session """ 
-        if input_sequence.isdigit() and len(input_sequence) == 4:
+        if input_sequence.isdigit() and len(input_sequence) == 4 and (len(set(input_sequence)) == 4):
             self.__players_sequence = input_sequence
-            self.__computers_sequence = 0  #self.__create_sequence()
+            self.__create_sequence()
             self.status = 'Game started'
         else: 
-            self.status = 'Wrong input. Value must contain 4 digits'
-    
+            self.status = 'Wrong input. Value must contain 4 unique digits'
+
+    def __create_sequence(self):
+        """ Generate __computer_sequence """ 
+        self.__computers_sequence = str()
+        while len(self.__computers_sequence) != 4:
+            rand = random.randint(0, 9)
+            if str(rand) not in self.__computers_sequence:
+                self.__computers_sequence  += str(rand)

9857f413cfcf15416d7319576f3f6e3ebc12b3ee
AveWycc220 <mik16748@gmail.com>
2020-05-20 18:46:07 +0300

    Fix (modules): Fix error with wrong value in check_value()

--- modules/bulls_and_cows/bulls_and_cows.py
+++ modules/bulls_and_cows/bulls_and_cows.py
@@ -1,34 +1,34 @@
 """ Module of class : BullsAndCows """
 import random
 
-
 SIZE = 4
 
 
-class BullsAndCows(): 
-    """ Class for playing in Bulls and Cows """ 
+class BullsAndCows():
+    """ Class for playing in Bulls and Cows """
     __computers_sequence = None
     __users_tries = None
     status = "Game is not initialized"
+
     def __init__(self):
-        """ Init game-session """ 
+        """ Init game-session """
         self.__create_sequence()
         self.__users_tries = 0
         self.status = 'Game started'
 
     def __create_sequence(self):
-        """ Generate __computer_sequence """ 
+        """ Generate __computer_sequence """
         self.__computers_sequence = str()
         while len(self.__computers_sequence) != SIZE:
             rand = random.randint(0, 9)
             if str(rand) not in self.__computers_sequence:
-                self.__computers_sequence  += str(rand)
+                self.__computers_sequence += str(rand)
 
     def check_value(self, value):
-        """ Check the value and output the number of cows and bulls """
-        if self.__is_correct(value):    
+        """ Check the value and output the number of cows and bulls. Value must be type == str """
+        if self.__is_correct(value) and self.__computers_sequence is not None:
             self.__users_tries += 1
-            bulls = 0 
+            bulls = 0
             cows = 0
             for i in range(0, SIZE):
                 if value[i] == self.__computers_sequence[i]:
@@ -36,6 +36,7 @@ class BullsAndCows():
             if bulls == SIZE:
                 self.status = F"--You won. Game end.--\nCount of tries = {self.__users_tries}\n\
 Start new game, if you want to continue."
+                self.__computers_sequence = None
                 return bulls, cows
             else:
                 for i in range(0, SIZE):
@@ -43,10 +44,13 @@ Start new game, if you want to continue."
                         cows += 1
                 self.status = F"Bulls = {bulls} | Cows = {cows}"
                 return bulls, cows
+        else:
+            return None, None
 
     def __is_correct(self, input_sequence):
-        """ Validation check """
-        if input_sequence.isdigit() and len(input_sequence) == SIZE and (len(set(input_sequence)) == SIZE):
+        """ Validation check. """
+        if isinstance(input_sequence, str) and input_sequence.isdigit() \
+                and len(input_sequence) == SIZE and (len(set(input_sequence)) == SIZE):
             return True
         else:
             self.status = F'Wrong input. Value must contain {SIZE} unique digits'

5ad174ebfb1531a6980519a151aedeb2ee4aa3f1
AveWycc220 <mik16748@gmail.com>
2020-05-25 15:15:31 +0300

    Feat (app): Add caching of all results

--- modules/api/app.py
+++ modules/api/app.py
@@ -1,25 +1,59 @@
 #!flask/bin/python
 """ Module for Flask APP """
-from datetime import datetime as dt
-from wsgiref.handlers import format_date_time
-from time import mktime
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort
 from flask_caching import Cache
 from modules.bulls_and_cows.bulls_and_cows import BullsAndCows
 
 app = Flask(__name__)
+config = {
+    "DEBUG": True,
+    "CACHE_TYPE": "simple",
+    "CACHE_DEFAULT_TIMEOUT": 86400,
+}
+app.config.from_mapping(config)
+cache = Cache(app)
 
 
 @app.route('/v1.0/start', methods=['GET'])
 def start():
-    global game 
+    global game
     game = BullsAndCows()
-    return jsonify({'status' : game.status, 'debug_info': game._BullsAndCows__computers_sequence})
+    results = get_all_results()
+    cache.clear()
+    cache.set('results', results)
+    if app.config['DEBUG']:
+        return jsonify({'status': game.status, 'debug_info': game.sequence})
+    else:
+        return jsonify({'status': game.status})
+
 
 @app.route('/v1.0/answer/<number>', methods=['GET'])
 def check(number):
-    answer = game.check_value(number)
-    if answer == (None, None): 
-        return jsonify({'status' : game.status})
-    else: 
-        return jsonify({'status' : game.status, 'bulls' : answer[0], 'cows' : answer[1], 'debug_info': game._BullsAndCows__computers_sequence})
+    if number in cache.get('results').keys():
+        answer = cache.get('results')[str(number)]
+        if answer == (None, None):
+            return jsonify({'status': game.status})
+        else:
+            if app.config['DEBUG']:
+                return jsonify(
+                    {'status': game.status, 'bulls': answer[0], 'cows': answer[1], 'debug_info': game.sequence})
+            else:
+                return jsonify({'status': game.status, 'bulls': answer[0], 'cows': answer[1]})
+    else:
+        return abort(404, description="Wrong number")
+
+
+def get_all_results():
+    sequence = game.sequence
+    all_options = list()
+    results = dict()
+    for i in range(0, 10):
+        for j in range(0, 10):
+            for k in range(0, 10):
+                for l in range(0, 10):
+                    if len(set(F'{i}{j}{k}{l}')) == 4:
+                        all_options.append(F'{i}{j}{k}{l}')
+    for i, item in enumerate(all_options):
+        temp = {item: BullsAndCows.check_value_static(item, sequence)}
+        results.update(temp)
+    return results

cb1cd5b25fe08e6ae9f267657c084af262d24233
AveWycc220 <mik16748@gmail.com>
2020-06-06 22:19:33 +0300

    feat (app): get rid of 'global game' and create class Game

--- modules/api/app.py
+++ modules/api/app.py
@@ -14,49 +14,57 @@ app.config.from_mapping(config)
 cache = Cache(app)
 
 
-@app.route('/v1.0/start', methods=['GET'])
-def start():
-    global game
-    game = BullsAndCows()
-    results = get_all_results()
-    cache.clear()
-    cache.set('results', results)
-    if app.config['DEBUG']:
-        return jsonify({'status': game.status, 'debug_info': game.sequence})
-    else:
-        return jsonify({'status': game.status})
+class Game:
+    """ Class for implementation bulls and cows in rest api"""
+    __game = None
 
-
-@app.route('/v1.0/answer/<number>', methods=['GET'])
-def check(number):
-    if cache.get('results'):
-        if number in cache.get('results').keys():
-            answer = cache.get('results')[str(number)]
-            if answer == (None, None):
-                return jsonify({'status': game.status})
-            else:
-                if app.config['DEBUG']:
-                    return jsonify(
-                        {'status': game.status, 'bulls': answer[0], 'cows': answer[1], 'debug_info': game.sequence})
-                else:
-                    return jsonify({'status': game.status, 'bulls': answer[0], 'cows': answer[1]})
+    @staticmethod
+    @app.route('/v1.0/start', methods=['GET'])
+    def start():
+        Game.__game = BullsAndCows()
+        results = Game.get_all_results()
+        cache.clear()
+        cache.set('results', results)
+        if app.config['DEBUG']:
+            return jsonify({'status': Game.__game.status, 'debug_info': Game.__game.sequence})
         else:
-            return abort(404, description="Wrong number")
-    else:
-        return abort(404, description=F"Game not started")
+            return jsonify({'status': Game.__game.status})
 
+    @staticmethod
+    @app.route('/v1.0/answer/<number>', methods=['GET'])
+    def check(number):
+        if cache.get('results'):
+            if number in cache.get('results').keys():
+                answer = cache.get('results')[str(number)]
+                if answer == (None, None):
+                    return jsonify({'status': Game.__game.status})
+                elif answer == (4, 0):
+                    Game.__game.check_value(f'{number}')
+                    cache.clear()
+                    return jsonify({'status': Game.__game.status})
+                else:
+                    if app.config['DEBUG']:
+                        return jsonify(
+                            {'status': Game.__game.status, 'bulls': answer[0], 'cows': answer[1], 'debug_info': Game.__game.sequence})
+                    else:
+                        return jsonify({'status': Game.__game.status, 'bulls': answer[0], 'cows': answer[1]})
+            else:
+                return abort(404, description="Wrong number")
+        else:
+            return abort(404, description="Game not started")
 
-def get_all_results():
-    sequence = game.sequence
-    all_options = list()
-    results = dict()
-    for i in range(0, 10):
-        for j in range(0, 10):
-            for k in range(0, 10):
-                for l in range(0, 10):
-                    if len(set(F'{i}{j}{k}{l}')) == 4:
-                        all_options.append(F'{i}{j}{k}{l}')
-    for i, item in enumerate(all_options):
-        temp = {item: BullsAndCows.check_value_static(item, sequence)}
-        results.update(temp)
-    return results
+    @staticmethod
+    def get_all_results():
+        sequence = Game.__game.sequence
+        all_options = list()
+        results = dict()
+        for i in range(0, 10):
+            for j in range(0, 10):
+                for k in range(0, 10):
+                    for l in range(0, 10):
+                        if len(set(F'{i}{j}{k}{l}')) == 4:
+                            all_options.append(F'{i}{j}{k}{l}')
+        for i, item in enumerate(all_options):
+            temp = {item: BullsAndCows.check_value_static(item, sequence)}
+            results.update(temp)
+        return results
